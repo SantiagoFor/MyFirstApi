ade59596e9a9573112eaf90e76cd7c48320fbebf
Santiago <santiagoforero561@gmail.com>
2026-05-24 18:11:04 -0500

    path file doc with coments 3 slash

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Scalar.AspNetCore;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +8,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlpath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlpath);
+}    
+);
+
+
 var MyAllowOrigins = "MyAllowOrigins";
 builder.Services.AddCors(options => {
     options.AddPolicy(name: MyAllowOrigins, p =>
